3d5ff83d9fa80ba0f2a7501173b1a22b90259a09
Sanchit Pandey <sanchitpandey72@gmail.com>
2020-10-29 21:47:54 +0530

    0

--- server.js
+++ server.js
@@ -5,6 +5,38 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
 const tokenUrl = "https://zoom.us/oauth/token/?";
+const zoom = "https://api.zoom.us/v2/users/";
+const webAPIKey = "AIzaSyD1i2UpM9PjXkd9uRyxC23DBPbRnnwugr4";
+const dynamicURL = `https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${webAPIKey}`;
+const templateMeeting = {
+  agenda: "string",
+  duration: 3600,
+  password: "string",
+  //schedule_for: "string",
+  settings: {
+    alternative_hosts: "",
+    approval_type: 2,
+    audio: "both",
+    auto_recording: "local",
+    cn_meeting: false,
+    enforce_login: false,
+    enforce_login_domains: "",
+    global_dial_in_countries: [null],
+    host_video: false,
+    in_meeting: false,
+    join_before_host: true,
+    mute_upon_entry: false,
+    participant_video: false,
+    registrants_email_notification: false,
+    registration_type: 2,
+    use_pmi: true,
+    watermark: true,
+  },
+  start_time: Date.now().toString,
+  timezone: "EST",
+  topic: "string",
+  type: 2,
+};
 var buffer = require("buffer").Buffer;
 
 app.set("view engine", "ejs");
@@ -36,15 +68,73 @@ app.get("/token", (req, res) => {
         if (err) res.send("Error");
         console.log(response);
         var json = JSON.parse(response.body);
-        //console.log("Access Token:", json.access_token);
-        res.status(200).send(json.access_token);
+        var token = json.access_token;
+        // Create Meeting
+        request.get(
+          {
+            url: zoom + "me",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer " + token,
+            },
+          },
+          (err, response) => {
+            if (err) {
+              console.log(err);
+            } else {
+              var json = JSON.parse(response.body);
+              var uid = json.account_id;
+              request.post(
+                {
+                  url: zoom + `me/meetings`,
+                  headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + token,
+                  },
+                  json: true,
+                  body: templateMeeting,
+                },
+                (err, response) => {
+                  if (err) {
+                    console.log(err);
+                    res.send("Error");
+                  } else {
+                    var url = response.body.start_url;
+                    //res.send("Success");
+                    request.post(
+                      {
+                        url: dynamicURL,
+                        body: JSON.stringify({
+                          dynamicLinkInfo: {
+                            domainUriPrefix: "https://doorapp.page.link",
+                            link: "https://www.example.com/?url=" + url,
+                            androidInfo: {
+                              androidPackageName: "com.sanchit.door_app",
+                            },
+                            iosInfo: {
+                              iosBundleId: "com.example.ios",
+                            },
+                          },
+                        }),
+                      },
+                      (err, response) => {
+                        if (err) {
+                          console.log(err);
+                        } else {
+                          var json = JSON.parse(response.body);
+                          res.redirect(json.shortLink);
+                        }
+                      }
+                    );
+                  }
+                }
+              );
+            }
+          }
+        );
       }
     );
   } else {
     console.log("2nd round");
   }
-});
-
-app.get("/", (req, res) => {
-  res.status(200).send(req.query);
 });
